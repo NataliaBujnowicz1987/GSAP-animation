028c37f5094c903fb6070f1829fb86291a7605d8
Natalia Bujnowicz <nataliabujnowicz1987@gmail.com>
2020-08-05 14:00:45 +0100

    Updates

---
+++ precache-manifest.d82a044d81d744ac9d1210058cfd13d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "581fc6ae4871c7e4bdadbd95a2e87a7a",
+    "url": "/GSAP-animation/index.html"
+  },
+  {
+    "revision": "4ec548a3590523a69e5a",
+    "url": "/GSAP-animation/static/css/main.16a2049a.chunk.css"
+  },
+  {
+    "revision": "9616868308b670127b48",
+    "url": "/GSAP-animation/static/js/2.d84688b5.chunk.js"
+  },
+  {
+    "revision": "2206f6f6cbbf99cae08d01b2952f71ad",
+    "url": "/GSAP-animation/static/js/2.d84688b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ec548a3590523a69e5a",
+    "url": "/GSAP-animation/static/js/main.7b15fb26.chunk.js"
+  },
+  {
+    "revision": "1147cca44f2befa9b6f5",
+    "url": "/GSAP-animation/static/js/runtime-main.67d4aa8b.js"
+  },
+  {
+    "revision": "6c574450c7024b300243144174f9cf38",
+    "url": "/GSAP-animation/static/media/scene.6c574450.svg"
+  }
+]);
